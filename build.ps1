e0900c1ad894db359613fd3d1fbb11d489a5b0f8
Alex Earl <slide.o.mix@gmail.com>
2019-07-08 09:47:12 -0700

    Fix paths

--- build.ps1
+++ build.ps1
@@ -8,8 +8,8 @@ Param(
 $builds = @{
     '' = @{ 'Dockerfile' = 'Dockerfile-windows' ; 'BuildArgs' = '' };
     'jdk11' = @{ 'Dockerfile' = 'Dockerfile-windows' ; 'BuildArgs' = '--build-arg `"JAVA_VERSION=11.0.3-1`" --build-arg `"JAVA_BASE_VERSION=11`" --build-arg "JAVA_ZIP_VERSION=11.0.3.7-1" --build-arg "JAVA_SHA256=b13703a7ee5ff3e14881b7d3488fa93942d1858ca3cd5fa9234b763df84dc937"'};
-    'nanoserver' = @{ 'Dockerfile' = 'Dockerfile-windows-nanoserver' ; 'BuildArgs' = '--build-arg "FINAL_CONTAINER_TYPE=nanoserver"' };
-    'nanoserver-jdk11' = @{ 'Dockerfile' = 'Dockerfile-windows-nanoserver' ; 'BuildArgs' = '--build-arg `"FINAL_CONTAINER_TYPE=nanoserver`" --build-arg `"JAVA_BASE_VERSION=11`" --build-arg "JAVA_VERSION=11.0.3-1" --build-arg "JAVA_ZIP_VERSION=11.0.3.7-1" --build-arg "JAVA_SHA256=b13703a7ee5ff3e14881b7d3488fa93942d1858ca3cd5fa9234b763df84dc937"' };
+    'nanoserver' = @{ 'Dockerfile' = 'Dockerfile-windows-nanoserver' ; 'BuildArgs' = '' };
+    'nanoserver-jdk11' = @{ 'Dockerfile' = 'Dockerfile-windows-nanoserver' ; 'BuildArgs' = '--build-arg `"JAVA_BASE_VERSION=11`" --build-arg "JAVA_VERSION=11.0.3-1" --build-arg "JAVA_ZIP_VERSION=11.0.3.7-1" --build-arg "JAVA_SHA256=b13703a7ee5ff3e14881b7d3488fa93942d1858ca3cd5fa9234b763df84dc937"' };
 }
 
 if(![System.String]::IsNullOrWhiteSpace($Build) -and ($builds.ContainsKey($Build) -or ($Build -eq 'default'))) {
