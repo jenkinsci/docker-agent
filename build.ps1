01e48ecf3a1a8181c20800328642865202731446
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-07-06 13:29:25 +0200

    fixup

--- build.ps1
+++ build.ps1
@@ -6,8 +6,8 @@ Param(
     [String] $RemotingVersion = '3131.vf2b_b_798b_ce99',
     [String] $BuildNumber = '1',
     [switch] $PushVersions = $false,
-    [switch] $DisableEnvProps = $false
-    [String] $AgentType = 'windows-2019',
+    [switch] $DisableEnvProps = $false,
+    [String] $AgentType = 'windows-2019'
 )
 
 $ErrorActionPreference = "Stop"
