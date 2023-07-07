a9a6873b28b6917056eef3be1b546040a8576fc8
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-07-07 19:16:52 +0200

    fixup(build): pass Window image tag as build arg for Pester docker tests

--- tests/test_helpers.psm1
+++ tests/test_helpers.psm1
@@ -115,7 +115,7 @@ function Run-Program($cmd, $params) {
     $stderr = $proc.StandardError.ReadToEnd()
     $proc.WaitForExit()
     if($proc.ExitCode -ne 0) {
-        Write-Host "`n`nstdout:`n$stdout`n`nstderr:`n$stderr`n$cmd`n`nparams:`n$param`n`n"
+        Write-Host "`n`nstdout:`n$stdout`n`nstderr:`n$stderr`n`ncmd:`n$cmd`n`nparams:`n$param`n`n"
     }
 
     return $proc.ExitCode, $stdout, $stderr
