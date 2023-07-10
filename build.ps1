7dfd8e3e72ce198c48392f757a81d2b86f12a88a
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-07-10 20:01:07 +0200

    debug prepared images list

--- build.ps1
+++ build.ps1
@@ -114,6 +114,11 @@ Invoke-Expression "$baseDockerCmd config --services" 2>$null | ForEach-Object {
     }
 }
 
+Write-Host '= PREPARE: Images list prepared:'
+Foreach($image in $builds) {
+    Write-Host "== $image (" + ($image.Tags - join ", ") + ")"
+}
+
 if(![System.String]::IsNullOrWhiteSpace($Build) -and $builds.ContainsKey($Build)) {
     Write-Host "= BUILD: Building image ${Build}..."
     $dockerBuildCmd = '{0} {1}' -f $baseDockerBuildCmd, $Build
