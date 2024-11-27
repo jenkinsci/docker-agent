fb1f092b266aa68cb9c380642b9d89bc4ba698c4
gounthar <gounthar@gmail.com>
2024-11-27 15:36:23 +0100

    fix(updatecli): Restarting from Damien's work.

--- updatecli/scripts/ubi9-latest-tag.sh
+++ updatecli/scripts/ubi9-latest-tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+
 # This script fetches the latest tag from the Red Hat Container Catalog API for UBI9 images.
 # It ensures that `jq` and `curl` are installed, fetches the tags, and processes them to find the unique tag.
 
@@ -12,25 +12,26 @@ URL="https://catalog.redhat.com/api/containers/v1/repositories/registry/registry
 # Check if jq and curl are installed
 # If they are not installed, exit the script with an error message
 if ! command -v jq >/dev/null 2>&1 || ! command -v curl >/dev/null 2>&1; then
-    echo "jq and curl are required but not installed. Exiting with status 1." >&2
+    >&2 echo "jq and curl are required but not installed. Exiting with status 1." >&2
     exit 1
 fi
 
 # Fetch the tags using curl
-response=$(curl -s "$URL" -H 'accept: application/json')
+# curl --silent --fail --location --header 'accept: application/json' "$URL"
+response=$(curl --silent --fail --location --verbose --header 'accept: application/json' "$URL")
 
 # Check if the response is empty or null
 if [ -z "$response" ] || [ "$response" == "null" ]; then
-  echo "Error: Failed to fetch tags from the Red Hat Container Catalog API."
+  >&2 echo "Error: Failed to fetch tags from the Red Hat Container Catalog API."
   exit 1
 fi
 
 # Parse the JSON response using jq to find the "latest" tag and its associated tags
-latest_tag=$(echo "$response" | jq -r '.data[].repositories[].signatures[] | select(.tags[] == "latest") | .tags[]')
+latest_tag="$(echo "$response" | jq '.data[].repositories[].signatures[].tags' | jq -s 'flatten(1) | first')"
 
 # Check if the latest_tag is empty
 if [ -z "$latest_tag" ]; then
-  echo "Error: No valid tags found."
+  >&2 echo "Error: No valid tags found."
   exit 1
 fi
 
@@ -39,4 +40,5 @@ unique_tag=$(echo "$latest_tag" | sort | uniq | grep -v latest | grep "-")
 
 # Trim spaces
 unique_tag=$(echo "$unique_tag" | xargs)
-echo $unique_tag
+echo "$unique_tag"
+exit 0
