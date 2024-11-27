6f93af74a60b4ba37980d858e2e43156637a97d5
Damien Duportal <damien.duportal@gmail.com>
2024-11-27 17:04:12 +0100

    code review

--- updatecli/scripts/ubi9-latest-tag.sh
+++ updatecli/scripts/ubi9-latest-tag.sh
@@ -17,27 +17,8 @@ if ! command -v jq >/dev/null 2>&1 || ! command -v curl >/dev/null 2>&1; then
 fi
 
 # Fetch the tags using curl
-response=$(curl --silent --fail --location --verbose --header 'accept: application/json' "$URL")
+curl --silent --fail --location --verbose --header 'accept: application/json' "$URL" \
+    | jq --sort-keys 'first(.data[].repositories[].signatures[].tags)[]') \
+    | xargs `# Trim eventual whitespaces`
 
-# Check if the response is empty or null
-if [ -z "$response" ] || [ "$response" == "null" ]; then
-  >&2 echo "Error: Failed to fetch tags from the Red Hat Container Catalog API."
-  exit 1
-fi
-
-# Parse the JSON response using jq to find the "latest" tag and its associated tags
-latest_tag="$(echo "$response" | jq --sort-keys 'first(.data[].repositories[].signatures[].tags)[]')"
-
-# Check if the latest_tag is empty
-if [ -z "$latest_tag" ]; then
-  >&2 echo "Error: No valid tags found."
-  exit 1
-fi
-
-# Sort and remove duplicates
-unique_tag=$(echo "$latest_tag" | sort | uniq | grep -v latest | grep "-")
-
-# Trim spaces
-unique_tag=$(echo "$unique_tag" | xargs)
-echo "$unique_tag"
 exit 0
