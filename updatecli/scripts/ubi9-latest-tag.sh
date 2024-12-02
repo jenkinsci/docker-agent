395163c3d837af9845086cd0240224ac0e390c01
Bruno Verachten <gounthar@gmail.com>
2024-12-02 15:17:20 +0100

    chore(updatecli): Adds documentation to the script and enhances the `jq` command.

--- updatecli/scripts/ubi9-latest-tag.sh
+++ updatecli/scripts/ubi9-latest-tag.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # This script fetches the latest tag from the Red Hat Container Catalog API for UBI9 images.
-# It ensures that `jq` and `curl` are installed, fetches the tags, and processes them to find the unique tag.
+# It ensures that `jq` and `curl` are installed, fetches the most recent tags, and processes them to find the unique tag associated to `latest`s.
 
 # The Swagger API endpoints for the Red Hat Container Catalog API are documented at:
 # https://catalog.redhat.com/api/containers/v1/ui/#/Repositories/graphql.images.get_images_by_repo
+
+# The script uses the following parameters for the API request:
+# - registry: registry.access.redhat.com
+# - repository: ubi9
+# - page_size: 100
+# - page: 0
+# - sort_by: last_update_date[desc]
+
+# The curl command fetches the JSON data containing the tags for the UBI9 images.
+# The script then parses the JSON response using jq to find the version associated with the "latest" tag.
+# It focuses on tags that contain a hyphen, as these represent the long-form tag names.
+# The script ensures that only one instance of each tag is kept, in case of duplicates.
 
 # Correct URL of the Red Hat Container Catalog API for UBI9
 URL="https://catalog.redhat.com/api/containers/v1/repositories/registry/registry.access.redhat.com/repository/ubi9/images?page_size=100&page=0&sort_by=last_update_date%5Bdesc%5D"
@@ -16,8 +28,13 @@ if ! command -v jq >/dev/null 2>&1 || ! command -v curl >/dev/null 2>&1; then
     exit 1
 fi
 
-# Fetch the tags using curl
-response=$(curl --silent --fail --location --connect-timeout 10 --max-time 30 --header 'accept: application/json' "$URL")
+# Fetch the tags using curl, for `ub9`, in the `registry.access.redhat.com`, sorted by last update date descending, and keeping only page 0.
+# --fail: Fail silently on HTTP errors
+# --silent: Don't show progress meter or error messages
+# --show-error: Show error message if it fails
+# --retry 3: Retry failed requests up to 3 times
+# --retry-delay 2: Wait 2 seconds between retries
+response=$(curl --silent --fail --location --connect-timeout 10 --retry 3 --retry-delay 2 --max-time 30 --header 'accept: application/json' "$URL")
 
 # Check if the response is empty or null
 if [ -z "$response" ] || [ "$response" == "null" ]; then
@@ -26,7 +43,7 @@ if [ -z "$response" ] || [ "$response" == "null" ]; then
 fi
 
 # Parse the JSON response using jq to find the version associated with the "latest" tag
-latest_tag=$(echo "$response" | jq -r '.data[].repositories[] | select(.tags[].name == "latest") | .tags[] | select(.name != "latest") | .name')
+latest_tag=$(echo "$response" | jq -r '.data[].repositories[] | select(.tags[].name == "latest") | .tags[] | select(.name != "latest" and (.name | contains("-"))) | .name' | sort -u)
 
 # Check if the latest_tag is empty
 if [ -z "$latest_tag" ]; then
@@ -34,11 +51,8 @@ if [ -z "$latest_tag" ]; then
   exit 1
 fi
 
-# Sort and remove duplicates
-unique_tag=$(echo "$latest_tag" | sort | uniq | grep -v latest | grep "-")
-
 # Trim spaces
-unique_tag=$(echo "$unique_tag" | xargs)
+unique_tag=$(echo "$latest_tag" | xargs)
 
 # Output the latest version
 echo "$unique_tag"
