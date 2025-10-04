0008a1efc5d935043d11c39c58b892107ccb6f43
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2025-10-05 00:17:49 +0200

    debug

--- tests/inbound-agent.Tests.ps1
+++ tests/inbound-agent.Tests.ps1
@@ -147,8 +147,10 @@ Describe "[$global:IMAGE_NAME] custom build args" {
         # try {
         #     Write-Host '[DEBUG] env:'
         #     docker exec $global:CONTAINERNAME $global:CONTAINERSHELL -c 'Get-ChildItem Env: | ForEach-Object { Write-Host "$($_.Name) = $($_.Value)" }'
-        #     Write-Host '[DEBUG] java -version:'
-        #     docker exec $global:CONTAINERNAME java -version
+            Write-Host '[DEBUG] java -version:'
+            docker exec $global:CONTAINERNAME java -version
+            Write-Host '[DEBUG] end java -version'
+
         #     Write-Host '[DEBUG] pwsh -c "java -version":'
         #     docker exec $global:CONTAINERNAME $global:CONTAINERSHELL -c "java -version"
         # } catch {}
