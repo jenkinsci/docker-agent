1178ca716bba6ff67764146a6c1139db17095726
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2025-10-05 02:00:04 +0200

    retry docker exec java -version with ErrorActionPreference = Continue

--- tests/inbound-agent.Tests.ps1
+++ tests/inbound-agent.Tests.ps1
@@ -147,9 +147,11 @@ Describe "[$global:IMAGE_NAME] custom build args" {
         # try {
         #     Write-Host '[DEBUG] env:'
         #     docker exec $global:CONTAINERNAME $global:CONTAINERSHELL -c 'Get-ChildItem Env: | ForEach-Object { Write-Host "$($_.Name) = $($_.Value)" }'
-            # Write-Host '[DEBUG] java -version:'
-            # Write-Host (docker exec $global:CONTAINERNAME java -version)
-            # Write-Host '[DEBUG] end java -version'
+        $ErrorActionPreference = 'Continue'
+        Write-Host '[DEBUG] java -version:'
+        Write-Host (docker exec $global:CONTAINERNAME java -version)
+        Write-Host '[DEBUG] end java -version'
+        $ErrorActionPreference = 'Stop'
 
         #     Write-Host '[DEBUG] pwsh -c "java -version":'
         #     docker exec $global:CONTAINERNAME $global:CONTAINERSHELL -c "java -version"
